0fd39754e7f7170f33acfc8e821f3f3142e0bdd2
hyun <0227tmdgus@naver.com>
2024-03-21 11:16:00 +0900

    Initial commit - Console 단위 로그 적용

---
+++ src/test/java/dev/hyun/logback/LogBackApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hyun.logback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
